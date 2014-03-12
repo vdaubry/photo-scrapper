380f893bcc3f3d6487f7e6a03bec0f7757c58b99
vincent daubry <vdaubry@gmail.com>
2014-03-12 19:37:21 +0100

    gestiond es erreurs mechanize

--- hosts/generic_host.rb
+++ hosts/generic_host.rb
@@ -10,8 +10,8 @@ class GenericHost
 
   def image_url
     puts "Parse images from #{URI.parse(@host_url).host}"
-
-    #begin
+    page_images = []
+    begin
       browser = Mechanize.new.get(@host_url)
       #page_images = browser.images_with(:src => /picture/, :mime_type => /jpg|jpeg|png/).reject {|s| %w(logo register banner).any? { |w| s.url.to_s.include?(w)}}
 
@@ -21,15 +21,10 @@ class GenericHost
         page_images.reject! {|s| %w(logo counter register banner imgbox.png thumbnail adhance offline medal top bottom male female promotext close btn home).any? { |w| s.url.to_s.include?(w)}}
       #end
       puts "No images found at : #{@host_url}" if page_images.blank?
-
-      page_images.first.url.to_s rescue nil
-
-    # rescue StandardError => e
-    #   puts "error = #{e.to_s} at page #{post_page.uri.to_s}"
-    #   Rails.logger.error e.to_s
-    #   page_images = []
-    # ensure
-    #   page_images.first
-    # end
+    rescue Mechanize::ResponseCodeError => e
+      puts "error = #{e.to_s} at page #{@host_url}"
+    ensure
+      return page_images.first.url.to_s rescue nil
+    end
   end
 end
