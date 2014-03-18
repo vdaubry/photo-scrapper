c417405217e61e6da6fec70c7eeb4ce0b087ab58
vincent daubry <vdaubry@gmail.com>
2014-03-18 08:14:12 +0100

    scrapping forum3

---
+++ spec/websites/forum3_spec.rb
@@ -0,0 +1,214 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'mechanize'
+require 'spec_helper'
+require_relative '../../websites/forum3'
+require_relative '../../models/post_api'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/cassette_library'
+  c.hook_into :webmock
+  c.default_cassette_options = { :record => :new_episodes }
+  c.configure_rspec_metadata!
+end
+
+describe "Forum3" do
+
+  let(:category_name) { YAML.load_file('config/forums.yml')["forum3"]["category1"] }
+  let(:date) { Date.parse("01/02/2010") }
+
+  before(:each) do
+    @url = YAML.load_file('config/forums.yml')["forum3"]["url"]
+    @forum3 = Forum3.new(@url)
+  end
+
+  def go_to_home_page
+    @forum3.home_page
+  end
+
+  def do_sign_in
+    @user = YAML.load_file('config/forums.yml')["forum3"]["username"]
+    @password = YAML.load_file('config/forums.yml')["forum3"]["password"]
+
+    @forum3.sign_in(@user, @password)
+  end
+
+  let(:forum_page) do
+    post_url = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["post_url"]
+    @forum3.current_page = Mechanize.new.get(post_url)
+    do_sign_in
+    @forum3.current_page
+  end
+
+  describe "unit tests", :local => :true do
+
+    describe "forum_topics", :vcr => true do
+      it "returns all topics" do
+        go_to_home_page
+        do_sign_in
+
+        forum_page = @forum3.current_page.link_with(:text => category_name).click
+        @forum3.forum_topics(forum_page).count.should == 74
+      end
+    end
+
+    describe "scrap_posts_from_category", :vcr => true do
+      it "iterates on all categories" do
+        go_to_home_page
+        do_sign_in
+        @forum3.expects(:scrap_post_hosted_images).times(74).returns(nil)
+
+        @forum3.scrap_posts_from_category(category_name, date)
+      end
+    end
+
+    describe "scrap_post_hosted_images", :vcr => true do
+      context "calls post api" do
+        it "creates a post" do
+          forum_page.stubs(:title).returns("foobar_2010_January")
+          PostApi.any_instance.expects(:create).with("52fd1d9a4d616303ef000000", "foobar_2010_January").returns(Post.new({"id" => "6789"}))
+          @forum3.stubs(:scrap_from_page).returns(nil)
+
+          @forum3.scrap_post_hosted_images(forum_page, date)
+        end
+
+        it "sets post_image_count to 0" do
+          PostApi.any_instance.stubs(:create).returns(Post.new({"id" => "6789"}))
+          @forum3.stubs(:scrap_from_page).returns(nil)
+
+          @forum3.scrap_post_hosted_images(forum_page, date)
+
+          @forum3.post_images_count.should == 0      
+        end
+
+        it "destroys post if post_image_count is 0" do
+          PostApi.any_instance.stubs(:create).returns(Post.new({"id" => "6789"}))
+          @forum3.stubs(:scrap_from_page).returns(nil)
+          @forum3.stubs(:post_images_count).returns(0)
+          PostApi.any_instance.expects(:destroy).with("52fd1d9a4d616303ef000000", "6789")
+          
+          @forum3.scrap_post_hosted_images(forum_page, date)
+        end
+
+        it "scrap post" do
+          PostApi.any_instance.stubs(:create).returns(Post.new({"id" => "6789"}))
+          @forum3.expects(:scrap_from_page).once.returns(nil)
+
+          @forum3.scrap_post_hosted_images(forum_page, date)
+        end
+      end
+    end
+
+    describe "host_urls", :vcr => true do
+      it "finds all images hosted urls" do
+        ImageApi.any_instance.stubs(:search).returns([])
+        res = @forum3.host_urls(forum_page)
+        res.count.should == 42
+      end
+    end
+
+    describe "direct urls", :vcr => true do
+      it "finds all direct images urls" do
+        post_url = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["scrap_from_page"]["tmp_hosted_post"]
+        @forum3.current_page = Mechanize.new.get(post_url)
+        do_sign_in
+        res = @forum3.direct_urls(@forum3.current_page)
+        res.count.should == 5
+      end
+
+      it "skips images that are links" do
+        post_url = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["post_url"]
+        @forum3.current_page = Mechanize.new.get(post_url)
+        do_sign_in
+        res = @forum3.direct_urls(@forum3.current_page)
+        res.count.should == 0
+      end      
+    end
+
+    describe "page_image_at_host_url", :vcr => true do
+      it "finds all images urls" do
+        host_url = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["host_url"]
+        expected_image = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["image_url"]
+        @forum3.page_image_at_host_url(host_url).url.to_s.should == expected_image
+      end
+    end
+
+    describe "scrap_from_page", :vcr => true do
+      let(:fake_host_url) { YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["host_url"] }
+
+      before(:each) do
+        @forum3.stubs(:go_to_next_page).returns(nil)
+      end
+
+      it "scraps hosted images" do
+        expected_image = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["image_url"]
+        @forum3.stubs(:host_urls).returns([fake_host_url])
+        @forum3.expects(:download_image).with(expected_image, anything).once.returns(nil)
+
+        @forum3.scrap_from_page(forum_page, date)
+      end
+
+      it "skips images not found" do
+        @forum3.stubs(:host_urls).returns([fake_host_url])
+        @forum3.stubs(:page_image_at_host_url).returns(nil)
+        @forum3.expects(:download_image).never
+
+        @forum3.scrap_from_page(forum_page, date)
+      end
+
+      it "scraps direct images" do
+        post_url = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["scrap_from_page"]["tmp_hosted_post"]
+        @forum3.current_page = Mechanize.new.get(post_url)
+        do_sign_in
+        tmp_hosted_image = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["scrap_from_page"]["tmp_hosted_image"]
+
+        @forum3.expects(:download_image).with(tmp_hosted_image, nil).once
+        @forum3.stubs(:download_image).with(Not(equals(tmp_hosted_image)), nil)
+
+        @forum3.scrap_from_page(@forum3.current_page, date)
+      end
+    end
+
+    describe "go_to_next_page", :vcr => true do
+      context "not yet scrapped" do
+        before(:each) do
+          PostApi.any_instance.stubs(:search).returns([])
+        end
+
+        # let(:forum_page) do
+        #   post_url = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["go_to_next_page"]["post1_url"]
+        #   @forum3.current_page = Mechanize.new.get(post_url)
+        #   do_sign_in
+        #   @forum3.current_page
+        # end        
+
+        it "updates post pages_url" do
+          expected_url = YAML.load_file('spec/websites/forums_test_conf.yml')["forum3"]["go_to_next_page"]["post_page2_url"]
+          @forum3.post_id = "456"
+          @forum3.stubs(:scrap_from_page).returns(nil)
+          PostApi.any_instance.expects(:update).with('52fd1d9a4d616303ef000000', "456", expected_url)
+          
+          @forum3.go_to_next_page(forum_page, date)
+        end
+
+        it "scraps next page" do
+          PostApi.any_instance.stubs(:search).returns([])
+          @forum3.expects(:scrap_from_page).once.returns(nil)
+          PostApi.any_instance.stubs(:update).returns(nil)
+          
+          @forum3.go_to_next_page(forum_page, date)
+        end
+      end
+
+      context "already scrapped" do
+        it "doesn't update post pages_url" do
+          PostApi.any_instance.stubs(:search).returns([Post.new({"id" => "6789"})])
+          @forum3.expects(:scrap_from_page).never
+          PostApi.any_instance.expects(:update).never
+
+          @forum3.go_to_next_page(forum_page, date)
+        end
+      end
+    end
+  end
+end
