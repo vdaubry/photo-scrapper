f2321641c7ebe5057cb1cfc04e7d541ffa423705
Vincent Daubry <vdaubry@gmail.com>
2014-04-23 07:11:34 +0200

    debug shutdown

--- websites/website_scrapper.rb
+++ websites/website_scrapper.rb
@@ -7,6 +7,7 @@ class WebsiteScrapper
 
   def start
     begin
+      fail = 1/0
       start_time = DateTime.now
       scrapping_date = @scrapper.scrapping_date
       pp "Start scrapping #{@scrapper.url} for month : #{scrapping_date}"
@@ -19,8 +20,11 @@ class WebsiteScrapper
       duration = DateTime.now-start_time
       pp "End scrapping #{@scrapper.url} with duration : #{duration}"
       Scrapping.update(@scrapper.id, scrapping.id, {:success => true, :duration => duration})
+    rescue StandardError => e
+      puts "Fail for unknown reason : "+e.to_s
     ensure
-      system("shutdown -h now") if APP_ENV['production']
+      puts "Shutting down in env #{APP_ENV}"
+      system("shutdown -h now") if APP_ENV == 'production'
     end
   end
 end
