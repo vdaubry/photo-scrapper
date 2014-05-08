3ceeb61a9f7d0de12d672a70217f4506ba52cb8e
Vincent Daubry <vdaubry@gmail.com>
2014-05-08 09:56:37 +0200

    suppression du redémarrage en fin de scrapping

--- websites/website_scrapper.rb
+++ websites/website_scrapper.rb
@@ -7,7 +7,7 @@ class WebsiteScrapper
   end
 
   def start
-    terminate_after do
+    Raven.capture do
       start_time = DateTime.now
       scrapping_date = @scrapper.scrapping_date
       pp "Start scrapping #{@scrapper.url} for month : #{scrapping_date}"
@@ -20,25 +20,6 @@ class WebsiteScrapper
       duration = DateTime.now-start_time
       pp "End scrapping #{@scrapper.url} with duration : #{duration}"
       Scrapping.update(@scrapper.id, scrapping.id, {:success => true, :duration => duration})
-      puts "about to shutdown"
-    end
-  end
-
-  def terminate_after
-    begin
-      Raven.capture do
-        yield
-      end
-    rescue StandardError => e
-      puts "Fail for unknown reason : "+e.to_s
-    ensure
-      if APP_ENV == 'production'
-        puts "Shutting down in env #{APP_ENV}"
-        system("shutdown -h now") 
-      else
-        puts "Error app env = #{APP_ENV}"
-        puts "is env production : #{APP_ENV == 'production'}"
-      end
     end
   end
 end
