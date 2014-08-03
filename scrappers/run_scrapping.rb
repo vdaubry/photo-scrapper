a496ac03f58b7dff5e70741bb80a9c12ed7f9b60
Vincent Daubry <vdaubry@gmail.com>
2014-08-03 18:37:00 +0200

    correction import des scrapper + conf CloudFormation

--- scrappers/run_scrapping.rb
+++ scrappers/run_scrapping.rb
@@ -4,7 +4,7 @@ require_relative 'scrapper_factory'
 ScrapperConf.load
 
 path = File.expand_path('../websites', File.dirname(__FILE__))
-Dir[path+"/*.rb"].each {|file| require file}
+Dir[path+"/**/*.rb"].each {|file| require file}
 
 puts "Start polling messages from website queue"
 Facades::SQS.new(ENV["WEBSITE_QUEUE_NAME"]).poll do |msg|
